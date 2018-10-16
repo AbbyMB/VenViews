d0c40c470cf983617f0606af8a2c7603b28ddbcf
Abigail Symes <abigailsymes17@gmail.com>
2018-10-16 10:00:29 -0400

    Submitting changes made on John's computer yesterday

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Venue.create( name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/' )
+Venue.create( name: 'Hatch Shell', address: '47 David G Mugar Way', description: 'The HALF shell', capacity: 500, url: 'http://www.hatchshell.com/' )
---
+++ spec/features/user_views_venues_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "visitor sees a list of venues" do
+  scenario "sees a list of venues" do
+    Venue.create(name: 'House Of Blues', address: '15 Lansdowne St', description: 'House Of Blues is awesome!', capacity: '2500', url: 'www.houseofblues.com')
+    visit venues_path
+
+    expect(page).to have_content "House Of Blues"
+  end
+
+end
