95ca7af1825328a2b376cd49ad1f6567a31d496d
Abby MB <abbymbardin@gmail.com>
2018-10-16 13:50:09 -0400

    Add unit test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  "add some examples to (or delete) #{__FILE__}"
+  user_new = User.new(email: "bob@bob.com", password: "888888")
+  no_email = User.new(password: "888888")
+  no_password = User.new(email: "bob@bob.com")
+
+  it "Is valid with attributes" do
+    expect(user_new).to be_valid
+  end
+
+  it "Is not valid without attributes" do
+    expect(no_email).to_not be_valid
+    expect(no_password).to_not be_valid
+  end
+
 end
