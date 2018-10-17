5747bd7d019660e85d00b1ef4f55496b3af94350
SeeSCode09 <seeshiphop@gmail.com>
2018-10-17 16:31:59 -0400

    CSS with passing tests

--- spec/features/user_views_venue_spec.rb
+++ spec/features/user_views_venue_spec.rb
@@ -8,7 +8,7 @@ feature 'user views individual venue', %Q{
 
 
   scenario 'present correct information' do
-    rock_roll = Venue.create(name: 'Rock and Roll', address: '6 Hamilton Lane', description: 'Rock and Roll all night long!', capacity: '5000', url: 'www.rockandroll.com')
+    rock_roll = Venue.create(name: 'Rock and Roll', address: '6 Hamilton Lane', description: 'Rock and Roll all night long!', capacity: '5000', url: 'www.rockandroll.com', image: 'https://d2uqqhmijd5j2z.cloudfront.net/files/131579/venue_gallery/HouseOfBluesBostonMA.jpg?1482355562')
     visit venue_path(rock_roll)
 
     expect(page).to have_content(rock_roll.name)
