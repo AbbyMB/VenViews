d9e19f5eebfd99953ec8c206b746b02fde1b4812
John Trocchi <jctbog9@aol.com>
2018-10-18 17:37:58 -0400

    added image to Venue and modified specs

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Venue, type: :model do
-  house_of_blues = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/' )
-  no_name = Venue.new( address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/' )
-  no_address = Venue.new( name: 'House of Blues', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/' )
-  no_description = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', capacity: 200, url: 'http://www.houseofblues.com/' )
-  no_capacity = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', url: 'http://www.houseofblues.com/' )
-  no_url = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200 )
+  house_of_blues = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/', image: 'http://www.thistestusedtowork.com' )
+  no_name = Venue.new( address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/', image: 'http://www.google.com' )
+  no_address = Venue.new( name: 'House of Blues', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/', image: 'http://www.website.com' )
+  no_description = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', capacity: 200, url: 'http://www.houseofblues.com/', image: 'http://www.image.com' )
+  no_capacity = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', url: 'http://www.houseofblues.com/', image: 'http://www.images.com' )
+  no_url = Venue.new( name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, image: 'http://www.poerpefjoih.com' )
 
   it "Is valid with attributes" do
     expect(house_of_blues).to be_valid
