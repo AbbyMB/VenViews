36b82905ddbd5996ed3eb59b649b605b132ac828
Abby MB <abbymbardin@gmail.com>
2018-10-18 15:53:32 -0400

    Add form to show page

---
+++ app/javascript/react/components/DescriptionField.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DescriptionField = props => {
+  return(
+    <label>
+      Description:
+      <input type="text" />
+    </label>
+  )
+
+}
+
+export default DescriptionField;
