6923135ffc1b8433bcdd06952b244e1e9d2ca788
John Trocchi <jctbog9@aol.com>
2018-10-19 09:56:14 -0400

    editted spec file and serializer to pass all tests

--- spec/controllers/api/v1/venues_controller_spec.rb
+++ spec/controllers/api/v1/venues_controller_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::VenuesController, type: :controller do
-  let!(:first_venue) { Venue.create(name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/') }
-  let!(:second_venue) { Venue.create(name: 'Hatch Shell', address: '47 David G Mugar Way', description: 'The HALF shell', capacity: 500, url: 'http://www.hatchshell.com/') }
-
+  let!(:first_venue) { Venue.create(name: 'House of Blues', address: '15 Lansdown Street', description: 'house o blues yo', capacity: 200, url: 'http://www.houseofblues.com/', image: 'http://www.image.com') }
+  let!(:second_venue) { Venue.create(name: 'Hatch Shell', address: '47 David G Mugar Way', description: 'The HALF shell', capacity: 500, url: 'http://www.hatchshell.com/', image: 'http://www.wayfair.com') }
+  
   describe "GET#index" do
     it "should return a list of all the venues" do
 
@@ -19,12 +19,14 @@ RSpec.describe Api::V1::VenuesController, type: :controller do
       expect(returned_json["venues"][0]["description"]).to eq "house o blues yo"
       expect(returned_json["venues"][0]["capacity"]).to eq 200
       expect(returned_json["venues"][0]["url"]).to eq "http://www.houseofblues.com/"
+      expect(returned_json["venues"][0]["image"]).to eq "http://www.image.com"
 
       expect(returned_json["venues"][1]["name"]).to eq "Hatch Shell"
       expect(returned_json["venues"][1]["address"]).to eq "47 David G Mugar Way"
       expect(returned_json["venues"][1]["description"]).to eq "The HALF shell"
       expect(returned_json["venues"][1]["capacity"]).to eq 500
       expect(returned_json["venues"][1]["url"]).to eq "http://www.hatchshell.com/"
+      expect(returned_json["venues"][1]["image"]).to eq "http://www.wayfair.com"
     end
   end
 end
