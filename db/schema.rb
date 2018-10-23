e115a1bfd69982c205b03518a4b1e97a5c261f81
Abby MB <abbymbardin@gmail.com>
2018-10-23 09:36:13 -0400

    migration after updating Ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_144832) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating", null: false
-    t.string "description", null: false
+    t.string "description"
     t.integer "vote_count", null: false
     t.bigint "user_id"
     t.bigint "venue_id"
